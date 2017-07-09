3b7fb4246b7e5edad3ce51081c2a3dd8fa1c656f
kanghuawu <kang-hua.wu@sjsu.edu>
2017-07-09 16:41:38 -0700

    change db setting

--- server/server/settings.py
+++ server/server/settings.py
@@ -85,16 +85,16 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'lb_server',
-#         'USER': 'lb_server',
-#         'PASSWORD': 'password',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lb_server',
+        'USER': 'lb_server',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
 
 # heroku db
 

f9dfe47519612d5c8d68ea28814331e1558324e6
sepehrtheraiss@gmail.com <sepehrtheraiss@gmail.com>
2017-07-18 16:58:49 -0700

    gets 3 recent activities

--- server/lbfe_app/api/views.py
+++ server/lbfe_app/api/views.py
@@ -145,7 +145,7 @@ class VolunteerActivitiesView(ListAPIView):
         volunteer_id = self.request.query_params.get('volunteer_id', None)
         print 'Volunteer ID: ' + volunteer_id
         if volunteer_id is not None:
-            queryset = Activity.objects.filter(volunteer_id=volunteer_id).order_by('volunteer_id')[:3] 
+            queryset = Activity.objects.filter(volunteer_id=volunteer_id).order_by('volunteer_id')[::-1][:3]
         return queryset
 
 

1a3a66df924a0da5d348fe144dad64ca7de10f6e
Sepehr <sepehrtheraiss@gmail.com>
2017-07-22 22:19:52 -0700

    modified 1

--- leaderboardServer/app/apps.py
+++ leaderboardServer/app/apps.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class LbfeAppConfig(AppConfig):
+class AppConfig(AppConfig):
     name = 'app'
--- leaderboardServer/leaderboardServer/settings.py
+++ leaderboardServer/leaderboardServer/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'app..model.volunteers'
 ]
 
 MIDDLEWARE_CLASSES = [

43e0ea7c05c58c21d2a21f8be34d1689c229f9cc
Sepehr <sepehrtheraiss@gmail.com>
2017-07-23 16:16:56 -0700

    utl

--- leaderboardServer/leaderboardServer/settings.py
+++ leaderboardServer/leaderboardServer/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'app..model.volunteers'
+    'app.model.volunteers'
 ]
 
 MIDDLEWARE_CLASSES = [

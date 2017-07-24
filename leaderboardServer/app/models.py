f84e05905a9eba69c0a7b1b94f51ee403bfecea9
Sepehr <sepehrtheraiss@gmail.com>
2017-07-24 01:07:13 -0700

    add unique name

--- leaderboardServer/app/models.py
+++ leaderboardServer/app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Volunteer(models.Model):
 	id = models.AutoField(primary_key=True) 
-	name = models.CharField(max_length=30)
+	name = models.CharField(unique=True,max_length=30)
 	points = models.IntegerField()
 	
 

dd8ffbf6788188a978618cae1d23371045087a77
sepehrtheraiss@gmail.com <sepehrtheraiss@gmail.com>
2017-07-23 21:30:47 -0700

    linked urls

--- leaderboardServer/app/api/urls.py
+++ leaderboardServer/app/api/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
-
+from views import VolunteerListAPIView
 urlpatterns = [
 	url(r'^volunteer/$',VolunteerListAPIView.as_view())
 ]

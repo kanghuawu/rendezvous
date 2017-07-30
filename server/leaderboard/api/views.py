ca3ade6fc9d017f680e18045015e5d6dbf3e6820
Sepehr <sepehrtheraiss@gmail.com>
2017-07-30 15:39:10 -0700

    modifications

--- server/leaderboard/api/views.py
+++ server/leaderboard/api/views.py
@@ -3,43 +3,11 @@ from __future__ import unicode_literals
 
 from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
-from activities.models import Activity, ActivityType
-from .serializers import ActivitySerializer, ActivityTypeSerializer
-from rest_framework.pagination import LimitOffsetPagination, PageNumberPagination
-from .pagination import PostPageNumberPagination
+from leaderboard.models import LeaderBoard
+from .serializers import LeaderBoardSerializer
 
 
-class ActivityListAPIView(ListAPIView, CreateAPIView):
-    queryset = Activity.objects.all()
-    serializer_class = ActivitySerializer
-    # def get_queryset(self, *args, **kwargs):
-    #     print Activity.objects.all()
-    #     queryset_list = Activity.objects.all().filter(user = self.request.user)
-    #     return queryset_list
+class LeaderBoardListAPIView(ListAPIView, CreateAPIView):
+    #queryset = LeaderBoard.objects.all()
+    serializer_class = LeaderBoardSerializer
 
-    pagination_class = PostPageNumberPagination
-    def perform_create(self, serializer):
-        serializer.save(volunteer = self.request.user)
-    def get_queryset(self, *args, **kwargs):
-        queryset_list = Activity.objects.all().filter(volunteer = self.request.user)
-        return queryset_list
-
-class ActivityDetailAPIView(RetrieveUpdateDestroyAPIView):
-    queryset = ActivityType.objects.all()
-    serializer_class = ActivityTypeSerializer
-    lookup_field = 'activity_id'
-    def get_queryset(self, *args, **kwargs):
-        queryset_list = Activity.objects.all().filter(volunteer = self.request.user)
-        return queryset_list
-
-
-class ActivityTypeListAPIView(ListAPIView, CreateAPIView):
-    queryset = ActivityType.objects.all()
-    serializer_class = ActivityTypeSerializer
-
-
-
-class ActivityTypeDetailAPIView(RetrieveUpdateDestroyAPIView):
-    queryset = ActivityType.objects.all()
-    serializer_class = ActivityTypeSerializer
-    lookup_field = 'type_id'
--- server/leaderboard/models.py
+++ server/leaderboard/models.py
@@ -7,18 +7,6 @@ from elders.models import Elder
 
 
 
-# Create your models here.
-class ActivityType(models.Model):
-    type_id = models.AutoField(primary_key=True)
-    type_name = models.CharField(max_length=30)
+# Create your database views here.
 
-
-class Activity(models.Model):
-    activity_id = models.AutoField(primary_key=True)
-    volunteer = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete = models.SET_NULL, null = True)
-    elder = models.ForeignKey(Elder, on_delete = models.SET_NULL, null = True)
-    activity_type = models.ForeignKey('ActivityType',on_delete = models.SET_NULL, null = True)
-    duration = models.SmallIntegerField()
-    date = models.DateField(auto_now_add=False, blank = True)
-    status = models.CharField(max_length=10, blank = True)
     

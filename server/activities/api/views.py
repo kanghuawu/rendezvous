72a7c26c1093271a9911fc195fc22adc6da5047b
Emily-Xue Kang <xkang3@dons.usfca.edu>
2017-08-07 23:51:08 -0700

    get hour counts

--- server/activities/api/views.py
+++ server/activities/api/views.py
@@ -10,6 +10,13 @@ from .pagination import PostPageNumberPagination
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED
 
+from datetime import timedelta
+import datetime
+from django.utils.timezone import now
+from django.db.models import Sum
+
+import itertools
+
 class ActivityCreateAPIView(CreateAPIView):
     queryset = Activity.objects.all()
     serializer_class = ActivityCreateSerializer
@@ -25,13 +32,34 @@ class ActivityListAPIView(ListAPIView):
     serializer_class = ActivitySerializer
     pagination_class = PostPageNumberPagination
     def get_queryset(self, *args, **kwargs):
+        oneweek = now() - timedelta(days=7)
+        onemonth = now() - timedelta(days=30)
+        #last_week_hours = Activity.objects.filter(date__gt=oneweek,volunteer=self.request.user).annotate(sum = Sum('duration')),
+        #last_month_hours = Activity.objects.filter(date__gt=onemonth,volunteer=self.request.user).annotate(sum = Sum('duration'))
+
+
+        last_week_hours = Activity.objects.filter(date__gt=oneweek,volunteer=self.request.user).aggregate(lastweek = Sum('duration'))
+        print last_week_hours
+        last_month_hours = Activity.objects.filter(date__gt=onemonth,volunteer=self.request.user).aggregate(lastmonth = Sum('duration'))
+        print last_month_hours
+
+        hourlist = [last_week_hours,last_month_hours]
+        print hourlist
+        # hourlist = (
+        #     Activity.objects.values('date').filter(date__gt=oneweek,volunteer=self.request.user).annotate(sum=Sum('duration')),
+        #     Activity.objects.values('date').filter(date__gt=onemonth,volunteer=self.request.user).annotate(sum=Sum('duration'))
+        # )
         queryset_list = Activity.objects.filter(volunteer = self.request.user).order_by('-date')
+        print queryset_list.__class__.__name__
         return queryset_list
+
+
 
 class ActivityDetailAPIView(RetrieveUpdateDestroyAPIView):
     queryset = ActivityType.objects.all()
-    serializer_class = ActivityTypeSerializer
+    serializer_class = ActivitySerializer
     lookup_field = 'activity_id'
+
     def get_queryset(self, *args, **kwargs):
         queryset_list = self.queryset.filter(volunteer = self.request.user)
         return queryset_list

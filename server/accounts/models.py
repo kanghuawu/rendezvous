7ff7f8ac78e9746171f4aa23b055a1cac70b2871
Emily-Xue Kang <xkang3@dons.usfca.edu>
2017-08-21 22:39:17 -0700

    increase hearts count once an activity is posted, update badge count by heart count

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -14,3 +14,21 @@ class Volunteer(AbstractEmailUser):
         if self.first_name == '' and self.last_name == '':
             return 'Anonymous'
         return '%s %s' % (self.first_name, self.last_name)
+
+    def increase_heart(self):
+        self.hearts = self.hearts + 1
+        self.save()
+
+    def give_badge(self):
+        if self.hearts >=10 and self.hearts <50:
+            self.badges = 1
+        elif self.hearts >=50 and self.hearts < 500:
+            self.badges = 2
+        elif self.hearts >=500 and self.hearts < 1000:
+            self.badges = 3
+        elif self.hearts >=1000 and self.hearts < 2000:
+            self.badges = 4
+        elif self.hearts >=2000:
+            self.badges = 5
+        self.save()
+        print self.badges

0d075c2cabecae2b6250924e0e696f2e910c7697
kanghuawu <kang-hua.wu@sjsu.edu>
2017-08-23 00:19:08 -0700

    enable debug

--- server/server/settings.py
+++ server/server/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g34q4=$ipkv*glj42f=+*zqwd5)ufclqoyy$2u!)8$3zy+fh(b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
